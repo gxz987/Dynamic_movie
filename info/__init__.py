3e352e865a48a585754690c27330c084d635cd8a
gxz <gxz987@qq.com>
2019-06-06 14:41:17 +0800

    使用工厂方法创建不同配置的app(db未完成)

--- info/__init__.py
+++ info/__init__.py
@@ -6,14 +6,19 @@ from flask_wtf import CSRFProtect
 from flask_session import Session
 
 
-app = Flask(__name__)
-# 1.集成配置类
-app.config.from_object(config["develop"])
-# 2.集成SQLAlchemy
-db = SQLAlchemy(app)
-# 3.集成redis
-redis_store = StrictRedis(host=config["develop"].REDIS_HOST, port=config["develop"].REDIS_PORT)
-# 4.集成CSRFProtect
-CSRFProtect(app)
-# 5.集成flask_session, Session 指定session的保存路径
-Session(app)
+# 只要是可变的参数：一、可以放在配置文件中，二、用函数封装 三、用全局变量
+# 所有可变的参数用函数的形参来代替
+def create_app(config_name):
+    app = Flask(__name__)
+    # 1.集成配置类
+    app.config.from_object(config[config_name])
+    # 2.集成SQLAlchemy
+    db = SQLAlchemy(app)
+
+    # 3.集成redis
+    redis_store = StrictRedis(host=config[config_name].REDIS_HOST, port=config[config_name].REDIS_PORT)
+    # 4.集成CSRFProtect
+    CSRFProtect(app)
+    # 5.集成flask_session, Session 指定session的保存路径
+    Session(app)
+    return app
--- manager.py
+++ manager.py
@@ -1,9 +1,8 @@
 from flask_script import Manager
 from flask_migrate import MigrateCommand, Migrate
-from info import app, db
+from info import create_app
 
-
-
+app = create_app("develop")
 # 6.集成flask_script
 manager = Manager(app)
 # 7.集成flask_migrate， 在flask中对数据库进行迁移
