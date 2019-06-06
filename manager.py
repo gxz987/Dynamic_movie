1f9ede071e6065cf2329c870ced47b4f86d4443a
gxz <gxz987@qq.com>
2019-06-06 11:17:31 +0800

    集成flask-migrate和flask-script

--- manager.py
+++ manager.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf import CSRFProtect
 from flask_session import Session
-
+from flask_script import Manager
+from flask_migrate import MigrateCommand, Migrate
 
 class Config(object):
+    SECRET_KEY = '12314326'
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/news'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
@@ -36,13 +38,19 @@ redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
 # 5.集成flask_session, Session 指定session的保存路径
 Session(app)
+# 6.集成flask_script
+manager = Manager(app)
+# 7.集成flask_migrate， 在flask中对数据库进行迁移
+Migrate(app, db)
+manager.add_command("db", MigrateCommand)
+
 
 
 @app.route('/')
 def index():
-    # redis_store.set("name", "xiaoxiao")
+    session["name"] = "hehehe"
     return 'ok'
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
