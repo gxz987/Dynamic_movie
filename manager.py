4046d395f413279c312963c4886560d181b7e8cc
gxz <gxz987@qq.com>
2019-06-06 10:32:08 +0800

    设置session的保存方式

--- manager.py
+++ manager.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf import CSRFProtect
+from flask_session import Session
 
 
 class Config(object):
@@ -11,6 +12,17 @@ class Config(object):
 
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
+
+    # 设置session的保存方式
+    SESSION_TYPE = 'redis'
+    # 指定session的储存对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 是否加密
+    SESSION_USE_SIGNER = True
+    # 设置是否永久保存
+    SESSION_PERMANENT = False
+    # 设置session的过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 app = Flask(__name__)
@@ -22,8 +34,8 @@ db = SQLAlchemy(app)
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 4.集成CSRFProtect
 CSRFProtect(app)
-
-
+# 5.集成flask_session, Session 指定session的保存路径
+Session(app)
 
 
 @app.route('/')
