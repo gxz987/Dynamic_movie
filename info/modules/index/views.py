00102c55e3ef7cb223afaa7a90b2175466dff579
gxz <gxz987@qq.com>
2019-06-07 15:22:24 +0800

    favicon.ico显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -12,10 +12,12 @@ def favicon():
     # 返回logn图片
     # 第一种方法：
     # return redirect("/static/news/favicon.ico")  # 有302重定向问题
+
     # 第二种方法
     return current_app.send_static_file("news/favicon.ico")
     # send_static_file(filename)
     # Function used internally to send static files from the static
     # folder to the browser.
+
     # 第三种方法
     # return send_file("static/news/favicon.ico")
