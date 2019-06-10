43c48eec3d410ba270d11849c98861caccb87e58
gxz <gxz987@qq.com>
2019-06-10 15:38:02 +0800

    新闻详情页面新闻相关内容的渲染

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,6 +1,6 @@
-from flask import render_template, session, current_app, g
+from flask import render_template, session, current_app, g, abort
 
-import constants
+from info import constants
 from info.utils.common import user_login
 from info.modules.news import news_blu
 from info.models import News
@@ -22,12 +22,31 @@ def detail(news_id):
         clicks_news = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
     except Exception as e:
         current_app.logger.error(e)
-
     clicks_news_li = [news.to_basic_dict() for news in clicks_news]
 
+    # 2.显示新闻的具体信息
+    # 判断新闻id是否存在
+    if not news_id:
+        abort(404)
+    # 判断新闻id类型是否是整型
+    try:
+        news_id = int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        abort(404)
+    # 通过新闻id查询新闻
+    news = None
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+    # 判断新闻是否存在
+    if not news:
+        abort(404)
 
     data = {
         "user_info":user.to_dict() if user else None,
-        "clicks_news_li":clicks_news_li
+        "clicks_news_li":clicks_news_li,
+        "news":news.to_dict()
     }
     return render_template("news/detail.html", data=data)
