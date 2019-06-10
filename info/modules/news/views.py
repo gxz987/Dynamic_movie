e3ff76c4ccb7347ecca2d1c8bc01e2b39311d8f3
gxz <gxz987@qq.com>
2019-06-10 11:47:55 +0800

    详情页用户信息显示(第一种方法)

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, session, current_app
 
 from info.modules.news import news_blu
+
 
 
 @news_blu.route("/<int:news_id>")
@@ -10,5 +11,16 @@ def detail(news_id):
     :param news_id:
     :return:
     """
-    data = {}
+    user_id = session.get("user_id")
+    user = None
+    if user_id:
+        try:
+            from info.models import User
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    data = {
+        "user_info":user.to_dict() if user else None
+    }
     return render_template("news/detail.html", data=data)
