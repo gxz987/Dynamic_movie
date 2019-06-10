f9f4fad80374c1cc3b6d5a42c19a7b0a39cad62c
gxz <gxz987@qq.com>
2019-06-10 14:30:47 +0800

    新闻详情页面右侧点击排行的显示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, session, current_app, g
 
+import constants
 from info.utils.common import user_login
 from info.modules.news import news_blu
-
+from info.models import News
 
 
 @news_blu.route("/<int:news_id>")
@@ -15,7 +16,18 @@ def detail(news_id):
     """
     user = g.user
 
+    # 1.显示新闻的点击排行
+    clicks_news = []
+    try:
+        clicks_news = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    clicks_news_li = [news.to_basic_dict() for news in clicks_news]
+
+
     data = {
-        "user_info":user.to_dict() if user else None
+        "user_info":user.to_dict() if user else None,
+        "clicks_news_li":clicks_news_li
     }
     return render_template("news/detail.html", data=data)
